cc318e9d87de2ce7d81e5d998a4aaa212140aa36
SupawitFocus <na.supawit_st@tni.ac.th>
2023-01-22 21:14:09 +0700

    update show history

--- src/component/BoardHistory.js
+++ src/component/BoardHistory.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Square from "./Square";
 
-const Board = ({ squares, onClick, gridC }) => (
+const Board = ({ squares, gridC }) => (
   <div className="boardH" style={{gridTemplateColumns: gridC}}>
     {squares.map((squares, i) => (
-      <Square key={i} value={squares} onClick={() => onClick(i)} />
+      <Square key={i} value={squares} />
     ))}
   </div>
 );
